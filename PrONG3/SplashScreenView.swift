6bcba8fa70702f8a4a2fef84e1536eba885b518c
Henrique Leal <henrique.leal135@gmail.com>
2024-04-18 17:53:37 -0300

    okook

---
+++ PrONG3/SplashScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  SplashScreenView.swift
+//  PrONG3
+//
+//  Created by HENRIQUE LEAL PEREIRA DOS SANTOS on 18/04/24.
+//
+
+import SwiftUI
+
+struct SplashScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashScreenView()
+}
