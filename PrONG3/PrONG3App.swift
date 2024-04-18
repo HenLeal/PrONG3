2d227f9cf49a5fe3b5b43b2bba2d1c4cb245ab59
Henrique Leal <henrique.leal135@gmail.com>
2024-04-18 17:51:33 -0300

    Initial Commit

---
+++ PrONG3/PrONG3App.swift
@@ -0,0 +1,17 @@
+//
+//  PrONG3App.swift
+//  PrONG3
+//
+//  Created by HENRIQUE LEAL PEREIRA DOS SANTOS on 18/04/24.
+//
+
+import SwiftUI
+
+@main
+struct PrONG3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
